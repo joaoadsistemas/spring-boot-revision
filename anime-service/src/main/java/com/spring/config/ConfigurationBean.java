6e8838700286428153f4b091b43abf924597c7ec
Joao Henrique <joaoadsistemas@gmail.com>
2025-07-16 13:26:03 -0300

    feat: ConfigurationProperties

--- anime-service/src/main/java/com/spring/config/ConfigurationBean.java
+++ anime-service/src/main/java/com/spring/config/ConfigurationBean.java
@@ -1,24 +1,22 @@
 package com.spring.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ConfigurationBean {
 
-    @Value("${database.username}")
-    private String username;
-
-    @Value("${database.url}")
-    private String url;
-
-    @Value("${database.port}")
-    private String port;
+    private final ConnectionConfigurationProperties configurationProperties;
 
     @Bean
     public ClassConfig configuration() {
-        return new ClassConfig(username, url, port);
+        return new ClassConfig(
+                configurationProperties.username(),
+                configurationProperties.url(),
+                configurationProperties.port());
     }
 
 }
