9a49653a311894694e810ccae97bb88534dc6e78
Joao Henrique <joaoadsistemas@gmail.com>
2025-07-17 11:18:46 -0300

    feat: rename data of UserRepositoryTest.java and finished UserService and UserServiceTest

---
+++ user-service/src/main/java/com/spring/user_service/service/UserService.java
@@ -0,0 +1,50 @@
+package com.spring.user_service.service;
+
+import com.spring.user_service.dto.UserGetResponseDTO;
+import com.spring.user_service.dto.UserPostRequestDTO;
+import com.spring.user_service.dto.UserPutRequestDTO;
+import com.spring.user_service.dto.UserPutResponseDTO;
+import com.spring.user_service.mapper.UserMapper;
+import com.spring.user_service.model.User;
+import com.spring.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private static final UserMapper MAPPER = UserMapper.INSTANCE;
+
+    public Set<UserGetResponseDTO> findAll() {
+        return userRepository.findAll().stream().map(MAPPER::toUserGetResponse).collect(Collectors.toSet());
+
+    }
+
+    public UserGetResponseDTO findById(Long id) {
+        return MAPPER.toUserGetResponse(userRepository.findById(id));
+    }
+
+    public UserGetResponseDTO findByEmail(String email) {
+        return MAPPER.toUserGetResponse(userRepository.findByEmail(email));
+    }
+
+    public void save(UserPostRequestDTO userPostRequestDTO) {
+        User user = MAPPER.toUser(userPostRequestDTO);
+        userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
+
+    public UserPutResponseDTO update(UserPutRequestDTO userPutRequestDTO) {
+        User user = MAPPER.toUser(userPutRequestDTO);
+        return MAPPER.toUserPutResponse(userRepository.update(user));
+    }
+
+}
--- user-service/src/test/java/com/spring/user_service/service/UserServiceTest.java
+++ user-service/src/test/java/com/spring/user_service/service/UserServiceTest.java
@@ -1,35 +1,33 @@
-package com.spring.user_service.repository;
+package com.spring.user_service.service;
 
-import com.spring.user_service.data.UserData;
 import com.spring.user_service.model.User;
+import com.spring.user_service.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-class UserRepositoryTest {
+class UserServiceTest {
 
     @InjectMocks
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Mock
-    private UserData userData;
+    private UserRepository userRepository;
 
     private Set<User> userSet = new HashSet<>();
-
 
     @BeforeEach
     void setUp() {
@@ -45,7 +43,7 @@ class UserRepositoryTest {
     @Test
     @DisplayName("findAll should return all elements when successfully")
     void findAll_shouldReturnAllElements_whenSuccessfully() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.when(userRepository.findAll()).thenReturn(userSet);
 
         var result = userRepository.findAll();
         assertThat(result).containsAll(userSet);
@@ -54,8 +52,8 @@ class UserRepositoryTest {
     @Test
     @DisplayName("findById should return user when successfully")
     void findById_shouldReturnUser_whenSuccessfully() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
         var expectedResult = userSet.stream().filter(u -> u.getId().equals(1L)).findFirst().get();
+        BDDMockito.when(userRepository.findById(ArgumentMatchers.anyLong())).thenReturn(expectedResult);
         var result = userRepository.findById(expectedResult.getId());
         assertThat(result).isNotNull().isEqualTo(expectedResult);
     }
@@ -63,7 +61,7 @@ class UserRepositoryTest {
     @Test
     @DisplayName("findById should throw an exception when id does not exists")
     void findById_shouldThrowException_whenIdDoesNotExists() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.when(userRepository.findById(ArgumentMatchers.anyLong())).thenThrow(ResponseStatusException.class);
         var id = 99L;
         assertThatThrownBy(() -> userRepository.findById(id)).isInstanceOf(ResponseStatusException.class);
     }
@@ -71,8 +69,8 @@ class UserRepositoryTest {
     @Test
     @DisplayName("findByEmail should return user when successfully")
     void findByEmail_shouldReturnUser_whenSuccessfully() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
         var expectedResult = userSet.stream().filter(u -> u.getId().equals(1L)).findFirst().get();
+        BDDMockito.when(userRepository.findByEmail(ArgumentMatchers.anyString())).thenReturn(expectedResult);
         var result = userRepository.findByEmail(expectedResult.getEmail());
         assertThat(result).isNotNull().isEqualTo(expectedResult);
     }
@@ -80,7 +78,7 @@ class UserRepositoryTest {
     @Test
     @DisplayName("findByEmail should throw an exception when email does not exists")
     void findByEmail_shouldThrowException_whenEmailDoesNotExists() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.when(userRepository.findByEmail(ArgumentMatchers.anyString())).thenThrow(ResponseStatusException.class);
         var email = "xaxa@gmail.com";
         assertThatThrownBy(() -> userRepository.findByEmail(email)).isInstanceOf(ResponseStatusException.class);
     }
@@ -88,17 +86,15 @@ class UserRepositoryTest {
     @Test
     @DisplayName("save should save a user when successfully")
     void save_shouldSaveUser_whenSuccessfully() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
         var expectedResult = User.builder().id(6L).firstName("Joao").lastName("Silva").email("joao.silva@example.com").build();
+        BDDMockito.doNothing().when(userRepository).save(ArgumentMatchers.any(User.class));
         assertThatNoException().isThrownBy(() -> userRepository.save(expectedResult));
-        assertThat(userRepository.findById(6L)).isEqualTo(expectedResult);
     }
 
     @Test
     @DisplayName("save should throw an exception when the email already exists")
     void save_shouldThrowAnException_whenEmailAlreadyExists() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
-
+        BDDMockito.doThrow(ResponseStatusException.class).when(userRepository).save(ArgumentMatchers.any(User.class));
         var user = userSet.stream().findFirst().get();
         user.setId(99L);
         user.setFirstName("Nicolas");
@@ -110,7 +106,7 @@ class UserRepositoryTest {
     @Test
     @DisplayName("save should throw an exception when the id already exists")
     void save_shouldThrowException_whenIdAlreadyExists() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.doThrow(ResponseStatusException.class).when(userRepository).save(ArgumentMatchers.any(User.class));
 
         var user = userSet.stream().findFirst().get();
         user.setEmail("nicolasmachado@gmail.com");
@@ -123,18 +119,17 @@ class UserRepositoryTest {
     @Test
     @DisplayName("delete should delete user when successfully")
     void delete_shouldDeleteUser_whenSuccessfully() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
-
         var removedUser = userSet.stream().findFirst().get();
 
+        BDDMockito.doNothing().when(userRepository).delete(ArgumentMatchers.anyLong());
+
         assertThatNoException().isThrownBy(() -> userRepository.delete(removedUser.getId()));
-        assertThat(userSet).doesNotContain(removedUser);
     }
 
     @Test
     @DisplayName("delete should throw an exception when id does not exists")
     void delete_shouldThrowAnException_whenIdDoesNotExists() {
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.doThrow(ResponseStatusException.class).when(userRepository).delete(ArgumentMatchers.anyLong());
         var id = 99L;
         assertThatThrownBy(() -> userRepository.delete(id)).isInstanceOf(ResponseStatusException.class);
     }
@@ -145,7 +140,7 @@ class UserRepositoryTest {
 
         var userBeforeModification = userSet.stream().findFirst().get();
 
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.when(userRepository.findById(userBeforeModification.getId())).thenReturn(userBeforeModification);
 
         var userResultBeforeModification = userRepository.findById(userBeforeModification.getId());
         assertThat(userResultBeforeModification).isNotNull().isEqualTo(userBeforeModification);
@@ -158,7 +153,7 @@ class UserRepositoryTest {
 
         userSet.add(userAfterModification);
 
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.when(userRepository.update(ArgumentMatchers.any(User.class))).thenReturn(userAfterModification);
 
         var userResultAfterModification = userRepository.update(userAfterModification);
 
@@ -171,7 +166,7 @@ class UserRepositoryTest {
     void put_shouldThrowAnException_whenIdDoesNotExists() {
 
         var user = User.builder().id(99L).email("xaxa").firstName("xaxa").lastName("xaxa").build();
-        BDDMockito.when(userData.getUsers()).thenReturn(userSet);
+        BDDMockito.when(userRepository.update(ArgumentMatchers.any(User.class))).thenThrow(ResponseStatusException.class);
 
         assertThatThrownBy(() -> userRepository.update(user)).isInstanceOf(ResponseStatusException.class);
     }
