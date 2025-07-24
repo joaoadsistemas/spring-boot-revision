5ada1c382d1f42e15ef2f359e162054016b6ab16
Joao Henrique <joaoadsistemas@gmail.com>
2025-07-24 11:36:39 -0300

    feat: solving n+1 with EntityGraph

--- user-service/src/main/java/com/spring/user_service/repository/UserProfileRepository.java
+++ user-service/src/main/java/com/spring/user_service/repository/UserProfileRepository.java
@@ -1,6 +1,8 @@
 package com.spring.user_service.repository;
 
 import com.spring.user_service.model.UserProfile;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,5 +14,7 @@ public interface UserProfileRepository extends JpaRepository<UserProfile, Long> 
     List<UserProfile> retrieveAll();
 
     @Override
+    //@EntityGraph(attributePaths = {"user", "profile"}) -> first way to use
+    @EntityGraph(value = "UserProfile.fullDetails")
     List<UserProfile> findAll();
 }
