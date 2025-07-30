92a384cb3b3ad28069c4c4603e766deba4b8bb95
Joao Henrique <joaoadsistemas@gmail.com>
2025-07-30 12:33:07 -0300

    feat: added csrf token

---
+++ user-service/src/main/java/com/spring/user_service/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.spring.user_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui.html",
+            "v3/api-docs/**",
+            "/swagger-ui/**",
+            "/csrf"
+    };
+
+    // authentication
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        var user = User.withUsername("user").password(passwordEncoder().encode("root")).roles("USER").build();
+        var admin = User.withUsername("admin").password(passwordEncoder().encode("root")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    // authorization
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(AUTH_WHITELIST).permitAll()
+                                .anyRequest().authenticated()).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
